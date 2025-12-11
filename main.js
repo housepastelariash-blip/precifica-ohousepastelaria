78e0b5f9971c07bdd1c9d261d6f5717adf8182c6
housepastelariash-blip <housepastelaria.sh@gmail.com>
2025-12-11 14:21:59 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+import React from "https://cdn.jsdelivr.net/npm/react@18.2.0/index.min.js";
+import ReactDOM from "https://cdn.jsdelivr.net/npm/react-dom@18.2.0/client.min.js";
+import App from "./App.js";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(React.createElement(App));
